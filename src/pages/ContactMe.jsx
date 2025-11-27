41ce7d462088a2d20e9a2981d2598ef0fa0cfb70
zsharon012 <zsharon012@gmail.com>
2025-11-27 00:05:30 -0600

    add base contact me page

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,11 +1,25 @@
 import MyDock from '../components/myDock';
+import { VscMail, VscGithubAlt } from "react-icons/vsc";
+import { FiLinkedin } from "react-icons/fi";
 
 export default function ContactMe() {
 
     return (
-        <>
-            <h1>contact me</h1>
+        <div className='contacts'>
+            <h1>Contact Me</h1>
+            <a href="mailto: zsharon012@gmail.com">
+                <VscMail size={40}/>
+                <h3>zsharon012@gmail.com</h3>
+            </a>
+            <a href="https://www.linkedin.com/in/zsharon012/">
+                <FiLinkedin size={40} />
+                <h3>www.linkedin.com/in/zsharon012</h3>
+            </a>
+            <a href="https://github.com/zsharon012">
+                <VscGithubAlt size={40} />
+                <h3>www.github.com/zsharon012</h3>
+            </a>
             <MyDock />
-        </>
+        </div>
     );
 }
