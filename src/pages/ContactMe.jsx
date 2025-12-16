5f44a6af70dbb1256c7b8eb268ac69478d15357c
zsharon012 <zsharon012@gmail.com>
2025-12-15 21:06:45 -0600

    fix where links open to

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -7,15 +7,27 @@ export default function ContactMe() {
     return (
         <div className='contacts'>
             <h1>Contact Me</h1>
-            <a href="mailto: zsharon012@gmail.com">
+            <a
+                href="mailto: zsharon012@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <VscMail size={40}/>
                 <h3>zsharon012@gmail.com</h3>
             </a>
-            <a href="https://linkedin.com/in/zsharon012/">
+            <a
+                href="https://linkedin.com/in/zsharon012/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <FiLinkedin size={40} />
                 <h3>www.linkedin.com/in/zsharon012</h3>
             </a>
-            <a href="https://github.com/zsharon012">
+            <a
+                href="https://github.com/zsharon012"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <VscGithubAlt size={40} />
                 <h3>www.github.com/zsharon012</h3>
             </a>
